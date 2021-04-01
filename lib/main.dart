02ca7a08cc95ac8411af1bc2932e9dee5fd95e0d
Bagas Hariyanto <bagashyt@gmail.com>
2021-04-01 17:39:55 +0700

    learn navigator push, pop

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learndart/login_page.dart';
 
 void main() {
   runApp(new MyApp());
@@ -8,24 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Aplikasi Hello World"),
-        ),
-        body: Center(
-            child: Container(
-                color: Colors.lightBlue,
-                width: 150,
-                height: 100,
-                child: Text(
-                  "Saya sedang melatih kemampuan flutter saya.",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20),
-                ))),
-      ),
+      home: LoginPage(),
     );
   }
 }
