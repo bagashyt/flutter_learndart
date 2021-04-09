ace3e81edde03311f56f5d034c725de358a96028
Bagas Hariyanto <bagashyt@gmail.com>
2021-04-10 01:54:26 +0700

    learn shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController(text: "No name");
+  bool isOn = false;
+
+  void saveData() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString("nama", controller.text);
+    pref.setBool("ison", isOn);
+  }
+
+  Future<String> getNama() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getString("nama") ?? "No Name";
+  }
+
+  Future<bool> getOn() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getBool("ison") ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Aplikasi Hello World"),
+          title: Text("Latihan Shared preferences"),
         ),
         body: Center(
-            child: Container(
-                color: Colors.lightBlue,
-                width: 150,
-                height: 100,
-                child: Text(
-                  "Saya sedang melatih kemampuan flutter saya.",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20),
-                ))),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              TextField(
+                controller: controller,
+              ),
+              Switch(
+                  value: isOn,
+                  onChanged: (newValue) {
+                    setState(() {
+                      isOn = newValue;
+                    });
+                  }),
+              RaisedButton(
+                  child: Text("Save"),
+                  onPressed: () {
+                    saveData();
+                  }),
+              RaisedButton(
+                  child: Text("Load"),
+                  onPressed: () {
+                    getNama().then((s) {
+                      controller.text = s;
+                      setState(() {});
+                      getOn().then((b) {
+                        isOn = b;
+                        setState(() {});
+                      });
+                    });
+                  }),
+            ],
+          ),
+        ),
       ),
     );
   }
