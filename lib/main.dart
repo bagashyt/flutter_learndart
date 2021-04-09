c3dee594ca935f432070ae810788de2aa7f5cb35
Bagas Hariyanto <bagashyt@gmail.com>
2021-04-10 04:11:51 +0700

    learn state management provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,59 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_learndart/application_color.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  bool isOn = true;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Aplikasi Hello World"),
+      home: ChangeNotifierProvider<ApplicationColor>(
+        // ignore: deprecated_member_use
+        builder: (context) => ApplicationColor(),
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Consumer<ApplicationColor>(
+              builder: (context, applicationColor, _) => Text(
+                "Privoder State Management",
+                style: TextStyle(color: applicationColor.color),
+              ),
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Consumer<ApplicationColor>(
+                  builder: (context, applicationColor, _) => AnimatedContainer(
+                    duration: Duration(milliseconds: 500),
+                    width: 100,
+                    height: 100,
+                    color: applicationColor.color,
+                    margin: EdgeInsets.all(5),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(margin: EdgeInsets.all(5), child: Text("AB")),
+                    Consumer<ApplicationColor>(
+                      builder: (context, applicationColor, _) => Switch(value: true, onChanged: (newValue) {
+                        applicationColor.isLightBlue = newValue;
+                    }),),
+                    Container(margin: EdgeInsets.all(5), child: Text("LB"))
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
-        body: Center(
-            child: Container(
-                color: Colors.lightBlue,
-                width: 150,
-                height: 100,
-                child: Text(
-                  "Saya sedang melatih kemampuan flutter saya.",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20),
-                ))),
       ),
     );
   }
