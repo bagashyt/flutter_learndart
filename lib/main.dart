761773b0a7df687a5305e497f57137208d699579
Bagas Hariyanto <bagashyt@gmail.com>
2021-04-13 01:01:55 +0700

    learn bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learndart/color_bloc.dart';
 
 void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ColorBloc bloc = ColorBloc();
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () {
+                bloc.eventSink.add(ColorEvent.to_amber);
+              },
+              backgroundColor: Colors.amber,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                bloc.eventSink.add(ColorEvent.to_light_blue);
+              },
+              backgroundColor: Colors.lightBlue,
+            )
+          ],
+        ),
         appBar: AppBar(
-          title: Text("Aplikasi Hello World"),
+          title: Text("BLoC tanpa Library"),
         ),
         body: Center(
-            child: Container(
-                color: Colors.lightBlue,
-                width: 150,
-                height: 100,
-                child: Text(
-                  "Saya sedang melatih kemampuan flutter saya.",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20),
-                ))),
+            child: StreamBuilder(
+          stream: bloc.stateStream,
+          initialData: Colors.amber,
+          builder: (context, snapshot) {
+            return AnimatedContainer(
+              width: 150,
+              height: 150,
+              color: snapshot.data,
+              duration: Duration(milliseconds: 1000),
+            );
+          },
+        )),
       ),
     );
   }
