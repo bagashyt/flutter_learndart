aaaa7d5095fc98f1beb5e8cb7c667d51ca4e3ded
Bagas Hariyanto <bagashyt@gmail.com>
2021-04-13 14:26:37 +0700

    learn card product cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learndart/product_card.dart';
+import 'package:flutter_learndart/product_state.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(new MyApp());
@@ -8,23 +11,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Aplikasi Hello World"),
+          backgroundColor: firstColor,
+          title: Text("Latihan Product Card"),
         ),
-        body: Center(
-            child: Container(
-                color: Colors.lightBlue,
-                width: 150,
-                height: 100,
-                child: Text(
-                  "Saya sedang melatih kemampuan flutter saya.",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20),
-                ))),
+        body: ChangeNotifierProvider<ProductState>(
+          // ignore: deprecated_member_use
+          builder: (context) => ProductState(),
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Consumer<ProductState>(
+                builder: (context, productState, _) => ProductCard(
+                  imageURL:
+                      "https://cdn-prod.medicalnewstoday.com/images/articles/308/308796/mixed-fruits.jpg",
+                  name: "Buah-buahan mix 1kg",
+                  price: "Rp 25.000",
+                  quantity: productState.quantity,
+                  notification:
+                      (productState.quantity > 5) ? "Diskon 10%" : null,
+                  onIncTap: () {
+                    productState.quantity++;
+                  },
+                  onDecTap: () {
+                    (productState.quantity >= 1)? productState.quantity-- : productState.quantity;
+                  },
+                  onAddCartTap: () {},
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
